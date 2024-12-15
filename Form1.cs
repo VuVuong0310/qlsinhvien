6738b7852d2b6871af7f8fad0d34cf53e38b40e0
VuVuong0310 <vuvanvuong2004@gmail.com>
2024-12-15 14:52:47 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lap03
+namespace Buoi_4
 {
     public partial class Form1 : Form
     {
@@ -17,207 +16,64 @@ namespace Lap03
         {
             InitializeComponent();
         }
-        int size = 14;
-        string font = "Tahoma";
-        private void AddFontInCMB()
+
+        private void btnThem_Click(object sender, EventArgs e)
         {
-            foreach (FontFamily font in new InstalledFontCollection().Families)
+            var form = new Form2();
+            if (form.ShowDialog() == DialogResult.OK)
             {
-                cbmFonts.Items.Add(font.Name);
+                // Thêm dữ liệu mới vào DataGridView
+                var nv = form.NhanVienMoi;
+                dgvNhanvien.Rows.Add(nv.MSNV, nv.TenNV, nv.LuongCB);
             }
         }
 
-        private void AddSizeInCMB()
+        private void btnSua_Click(object sender, EventArgs e)
         {
-            int[] sizes = { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 50, 72 };
-
-            foreach (int size in sizes)
+            if (dgvNhanvien.CurrentRow != null)
             {
-                cmbSizes.Items.Add(size);
+                // Lấy dữ liệu dòng được chọn
+                int index = dgvNhanvien.CurrentRow.Index;
+                var nv = new Nhanvien
+                {
+                    MSNV = dgvNhanvien.Rows[index].Cells[0].Value.ToString(),
+                    TenNV = dgvNhanvien.Rows[index].Cells[1].Value.ToString(),
+                    LuongCB = Convert.ToDouble(dgvNhanvien.Rows[index].Cells[2].Value)
+                };
+
+                // Mở form Nhân viên để sửa
+                var form = new Form2(nv);
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    // Cập nhật dữ liệu đã chỉnh sửa vào DataGridView
+                    var nvMoi = form.NhanVienMoi;
+                    dgvNhanvien.Rows[index].Cells[0].Value = nvMoi.MSNV;
+                    dgvNhanvien.Rows[index].Cells[1].Value = nvMoi.TenNV;
+                    dgvNhanvien.Rows[index].Cells[2].Value = nvMoi.LuongCB;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
-        private void dinhDangToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnXoa_Click(object sender, EventArgs e)
         {
-            FontDialog fontDlg = new FontDialog();
-            fontDlg.ShowColor = true;
-            fontDlg.ShowApply = true;
-            fontDlg.ShowEffects = true;
-            fontDlg.ShowHelp = true;
-
-            if (fontDlg.ShowDialog() == DialogResult.OK)
+            if (dgvNhanvien.CurrentRow != null)
             {
-                richTextBox1.ForeColor = fontDlg.Color;
-                richTextBox1.Font = fontDlg.Font;
+                // Xóa dòng được chọn
+                dgvNhanvien.Rows.RemoveAt(dgvNhanvien.CurrentRow.Index);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
-        private void Lap03_02_Load(object sender, EventArgs e)
-        {
-            AddFontInCMB();
-            AddSizeInCMB();
-            lamMoiRich();
-        }
-
-        private void lamMoiRich()
-        {
-            richTextBox1.Clear();
-            richTextBox1.Font = new Font("Tahoma", 14, FontStyle.Regular);
-            cbmFonts.SelectedItem = "Tahoma";
-            cmbSizes.SelectedItem = 14;
-        }
-
-        private void cmbSizes_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            size = Int32.Parse(cmbSizes.Text);
-            richTextBox1.Font = new Font(font, size, FontStyle.Regular);
-        }
-
-        private void cbmFonts_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            font = cbmFonts.Text;
-            richTextBox1.Font = new Font(font, size);
-        }
-
-        private void thoatToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnKT_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void taoVanBanMoiToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            lamMoiRich();
-        }
-
-        private void toolStripButton1_Click(object sender, EventArgs e)
-        {
-            lamMoiRich();
-        }
-
-        private void moTapTinToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-
-            ofd.Filter = "Text file | *.txt | RFT File | *.rft";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                string selectedFileName = ofd.FileName;
-                richTextBox1.LoadFile(selectedFileName, RichTextBoxStreamType.UnicodePlainText);
-
-            }
-        }
-
-        private void Lap03_02_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Control && e.KeyCode == Keys.N)
-            {
-                lamMoiRich();
-                e.Handled = true;
-                return;
-            }
-            if (e.Control && e.KeyCode == Keys.O)
-            {
-                moTapTinToolStripMenuItem_Click(this, e);
-                e.Handled = true;
-                return;
-            }
-            if (e.Control && e.KeyCode == Keys.S)
-            {
-                toolStripButton2_Click(this, e);
-                e.Handled = true;
-                return;
-            }
-        }
-
-        private void toolStripButton2_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.CheckPathExists = true;
-            saveFileDialog.Title = "Lưu tập tin văn bản";
-            saveFileDialog.DefaultExt = "rtf";
-            saveFileDialog.Filter = "RichText files|*.rtf";
-            saveFileDialog.RestoreDirectory = true;
-            saveFileDialog.AddExtension = true;
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                string selectedFileName = saveFileDialog.FileName;
-                try
-                {
-                    richTextBox1.SaveFile(selectedFileName, RichTextBoxStreamType.UnicodePlainText);
-                    MessageBox.Show("Tập tin đã được lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Đã xảy ra lỗi trong quá trình lưu tập tin: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void luuNoiDungVanBanToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.CheckPathExists = true;
-            saveFileDialog.Title = "Lưu tập tin văn bản";
-            saveFileDialog.DefaultExt = "txt";
-            saveFileDialog.Filter = "Text File|*.txt";
-            saveFileDialog.RestoreDirectory = true;
-            saveFileDialog.AddExtension = true;
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                string selectedFileName = saveFileDialog.FileName;
-                try
-                {
-                    richTextBox1.SaveFile(selectedFileName, RichTextBoxStreamType.UnicodePlainText);
-                    MessageBox.Show("Tập tin đã được lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Đã xảy ra lỗi trong quá trình lưu tập tin: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void toolStripButton3_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.SelectionFont.Bold)
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style & ~FontStyle.Bold);
-            }
-            else
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style | FontStyle.Bold);
-            }
-        }
-
-        private void toolStripButton4_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.SelectionFont.Italic)
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style & ~FontStyle.Italic);
-            }
-            else
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style | FontStyle.Italic);
-            }
-        }
-
-        private void toolStripButton5_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.SelectionFont.Underline)
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style & ~FontStyle.Underline);
-            }
-            else
-            {
-                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, richTextBox1.SelectionFont.Style | FontStyle.Underline);
-            }
-        }
-
-        private void cbmFonts_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
---
+++ Form2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi_4
+{
+    public partial class Form2 : Form
+    {
+        public Nhanvien NhanVienMoi { get; private set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(Nhanvien nhanVien)
+        {
+            InitializeComponent();
+
+            txtMa.Text = nhanVien.MSNV;
+            txtTen.Text = nhanVien.TenNV;
+            txtLuong.Text = nhanVien.LuongCB.ToString();
+
+            NhanVienMoi = nhanVien;
+        }
+
+        private void btnDongy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMa.Text) ||
+                   string.IsNullOrWhiteSpace(txtTen.Text) ||
+                   string.IsNullOrWhiteSpace(txtLuong.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.", "Thông báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!double.TryParse(txtLuong.Text, out double luongCB))
+            {
+                MessageBox.Show("Lương cơ bản phải là số.", "Thông báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            NhanVienMoi = new Nhanvien
+            {
+                MSNV = txtMa.Text.Trim(),
+                TenNV = txtTen.Text.Trim(),
+                LuongCB = luongCB
+            };
+
+            DialogResult = DialogResult.OK;
+            Close();
+
+        }
+
+        private void btnBoqua_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
